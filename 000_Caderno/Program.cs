449ce6088498263e5fbf37d6f3e8c424088aa8b7
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-09-15 19:49:30 -0300

    Criado projeto 000: Caderno para Testes

---
+++ 000_Caderno/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _000_Caderno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
